4f61d8462e0dcd1ef4f3cabb14e05c4ae8732560
aloima <acar.aloima@gmail.com>
2024-06-08 10:31:02 +0300

    fix invalid finding ending characters for char_at method

--- libs/utils/at.c
+++ libs/utils/at.c
@@ -2,6 +2,8 @@
 
 int char_at(char *data, const char ch) {
 	char *b = data;
-	while (*data++ != ch && *data != '\0');
-	return (*data == '\0') ? -1 : (data - b - 1);
+	while (*data != ch && *data != '\0') data++;
+
+	const unsigned int diff = (data - b);
+	return b[diff] == '\0' ? -1 : diff;
 }
