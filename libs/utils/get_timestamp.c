497119c705846e6454f85c650cd9e7f641658ff9
aloima <acar.aloima@gmail.com>
2026-05-17 22:55:50 +0300

    fix: invalid time calculating on `get_timestamp()` method

--- libs/utils/get_timestamp.c
+++ libs/utils/get_timestamp.c
@@ -6,7 +6,7 @@ int64_t get_timestamp() {
     if (clock_gettime(CLOCK_REALTIME, &ts) == -1)
       return -1;
 
-    return (ts.tv_sec * 1000) + (ts.tv_nsec / 1000);
+    return (ts.tv_sec * 1000) + (ts.tv_nsec / 1000000);
   #elif defined(__WIN32__)
     struct _timeb timestamp;
     if (_ftime_s(&timestamp) != 0)
