f3689284d0511f55db8ce70784e85553edda7696
aloima <acar.aloima@gmail.com>
2026-06-25 18:34:43 +0300

    refactor: stack overflow potential on freeing json element

--- libs/json/free.c
+++ libs/json/free.c
@@ -1,49 +1,63 @@
 #include <shivers.h>
 
 static void free_element(jsonelement_t *element) {
-  if (element) {
-    if (element->key) {
-      free(element->key);
-    }
+  ASSERT(element, !=, NULL);
 
-    if (element->value) {
-      free(element->value);
-    }
-
-    free(element);
+  if (element->key) {
+    free(element->key);
   }
+
+  if (element->value) {
+    free(element->value);
+  }
+
+  free(element);
 }
 
 static void free_elements(jsonelement_t *parent) {
-  if (parent) {
-    if (parent->type == JSON_ARRAY || parent->type == JSON_OBJECT) {
-      for (unsigned int i = 0; i < parent->size; ++i) {
-        jsonelement_t *element = ((jsonelement_t **) parent->value)[i];
+  ASSERT(parent, !=, NULL);
 
-        if (element) {
-          if (element->type == JSON_ARRAY || element->type == JSON_OBJECT) {
-            free_elements(element);
-          } else {
-            free_element(element);
-          }
+  uint32_t capacity = 128;
+  jsonelement_t **stack = allocate(NULL, 0, capacity, sizeof(jsonelement_t *));
+  if (stack == NULL)
+    return;
+
+  uint32_t top = 0;
+  stack[top++] = parent;
+
+  while (top > 0) {
+    jsonelement_t *current = stack[--top];
+
+    if (current->type == JSON_ARRAY || current->type == JSON_OBJECT) {
+      jsonelement_t **elements = (jsonelement_t **) current->value;
+
+      for (uint32_t i = 0; i < current->size; ++i) {
+        jsonelement_t *element = elements[i];
+        if (top >= capacity) {
+          stack = allocate(stack, capacity, capacity * 2, sizeof(jsonelement_t *));
+          capacity *= 2;
+
+          if (stack == NULL)
+            return;
         }
+
+        stack[top++] = element;
       }
     }
 
-    free_element(parent);
+    free_element(current);
   }
+
+  free(stack);
 }
 
 void json_free(jsonelement_t *element, const bool all) {
   jsonelement_t *top = element;
+  ASSERT(element, !=, NULL);
 
   if (all) {
-    while (true) {
-      if (top->parent) {
-        top = top->parent;
-      } else {
-        break;
-      }
+    while (top->parent) {
+      top = top->parent;
     }
   }
 
