90bbd5233df7fce9258857e1bdc8a4ee4e514171
aloima <acar.aloima@gmail.com>
2026-07-19 00:23:15 +0300

    refactor(utils): add assertion into allocate method

--- libs/utils/allocate.c
+++ libs/utils/allocate.c
@@ -2,8 +2,10 @@
 
 void *allocate(void *value, const long old_count, const unsigned long new_count, const unsigned char size) {
   if (value == NULL) {
-    if (old_count < 0) return malloc(new_count * size);
-    else return calloc(new_count, size);
+    if (old_count < 0)
+      return malloc(new_count * size);
+    else
+      return calloc(new_count, size);
   } else {
     void *val = realloc(value, new_count * size);
 
@@ -11,7 +13,7 @@ void *allocate(void *value, const long old_count, const unsigned long new_count,
       const long diff = (new_count - old_count);
 
       if (diff > 0) {
-        memset(val + (old_count * size), 0, size * diff);
+        ASSERT(memset(val + (old_count * size), 0, size * diff), !=, NULL);
       }
     }
 
